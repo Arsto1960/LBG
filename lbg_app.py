01955f6596654b3cc44480e938975a105cbeb83e
Mustafa <90824284+Arsto1960@users.noreply.github.com>
2025-12-20 17:50:14 +0100

    Fix HTML formatting for metrics display

--- lbg_app.py
+++ lbg_app.py
@@ -126,7 +126,7 @@ with col_controls:
     st.markdown(f"""
     <div class="metric-box">
         # <h3>N = {current_N}</h3>
-        <b>N =</b><{current_N}>
+        <b>N = {current_N}</b><br>
         <small>Target: {target_size}</small><br><br>
         <b>Distortion (MSE):</b><br>
         <span style="font-size: 1.2em; color: #ff4b4b;">{distortion:.4f}</span>
